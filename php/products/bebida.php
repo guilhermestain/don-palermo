8e2c3626613af38f756241f0ecd79f9a686d93b8
alexandrejuk <ale_santos.soares@hotmail.com>
2018-07-24 20:20:31 -0300

    order by  products asc

--- php/products/bebida.php
+++ php/products/bebida.php
@@ -5,7 +5,7 @@ require_once('../config/db.config.php');
 $dbConnection = new ConfigDb();
 $link = $dbConnection->connectDb();
 
-$sql = "select * from produtos where categoria = 'BEBIDA' order by id asc";
+$sql = "select * from produtos where categoria = 'BEBIDA' order by descricao asc";
 
 $result = mysqli_query($link, $sql);
 
