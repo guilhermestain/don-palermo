c7475edf43f9e612481932bdc5d92d24bb01c949
alexandrejuk <ale_santos.soares@hotmail.com>
2018-07-25 14:18:34 -0300

    created logic to add product

--- php/cart/index.php
+++ php/cart/index.php
@@ -1,24 +1,60 @@
 <?php
-  session_start();
+require_once('../config/db.config.php');
   
-  $produto_id = isset($_GET['id']) ? $_GET['id'] : null;
+$dbConnection = new ConfigDb();
+$link = $dbConnection->connectDb();
 
-  if(isset($_SESSION['shopping_cart'])) {
+$cart = isset($_SESSION['shopping_cart']) 
+? array_filter($_SESSION['shopping_cart'], function($data) {
+  return $data > 0;
+  }) 
+: $_SESSION['shopping_cart'] = array();
 
-    if(count($_SESSION['shopping_cart']) === 0) {
+$cartFormat = "(".implode(',',$cart).")";
 
-      array_push($_SESSION['shopping_cart'], $produto_id);
+$sql = "select * from produtos where id IN (".implode(',',$cart).")";
 
-    }else {
+$produtos = array();
+$result = mysqli_query($link, $sql);
 
-      array_push($_SESSION['shopping_cart'], $produto_id);
-
-    }
-
-  }else {
-
-    $_SESSION['shopping_cart'] = array();   
-
+if(count($cart)) {
+  while($produto = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    $produtos[] = $produto;
   }
-  
+}
+
+
+  // como criar filtros com php
+      // $numbers = array(1,2,3,4,5,6,7,8,25, 'alexandre');
+
+    // $more_than_four = array_filter($numbers, function($data) {
+    //   return $data >= 4;
+    // });
+
+    // $less_than_four = array_filter($numbers, function($data) {
+    //   return $data < 4;
+    // });
+
+    // $equal = array_filter($numbers, function($data) {
+    //   return $data === 'alexandre';
+    // });
+
+    // $equalTo = array_filter($numbers, function($data) {
+    //   return $data === 25;
+    // });
+
+
+
+    // print_r($more_than_four);
+    // echo '<br><br>';
+    // print_r($less_than_four);
+    // echo '<br><br>';
+    // print_r($equal);
+    // echo '<br><br>';
+    // print_r($equalTo);
+    // echo '<br><br>';
+    // print($produto_id);
+
 ?>
+
+
